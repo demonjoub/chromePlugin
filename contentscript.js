0b0d6dfe20eef2a4d6266260d61936c22fc47798
demonjoub <demonjoub@gmail.com>
2019-02-05 00:41:02 +0700

    Update contentscript.js
    check video auto play

--- contentscript.js
+++ contentscript.js
@@ -1,10 +1,14 @@
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-  // alert('updated from contentscript');
-// });
-console.log("get currentpage", window.location.href);
+// // chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+//   // alert('updated from contentscript');
+// // });
+// console.log("get currentpage", window.location.href);
 
 var vid = $('video').get(0);
 vid.addEventListener('ended', function (e) {
-  console.log('The video ended!');
+  vid.play();
+  alert('end');
 });
-console.log(vid)
+console.log(vid);
+
+// check this https://www.youtube.com/watch?v=lTTajzrSkCw
+// try again
